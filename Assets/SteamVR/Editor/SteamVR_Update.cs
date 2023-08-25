b8e82f62dfb517984a2c5aca95b255566fef1ade
Keith Bradner <keithb@valvesoftware.com>
2023-08-25 13:50:00 -0400

    Update urls to stop console spam on unity 2022

--- Assets/SteamVR/Editor/SteamVR_Update.cs
+++ Assets/SteamVR/Editor/SteamVR_Update.cs
@@ -19,9 +19,9 @@ namespace Valve.VR
     public class SteamVR_Update : EditorWindow
     {
         const string currentVersion = "2.1";
-        const string versionUrl = "http://media.steampowered.com/apps/steamvr/unitypluginversion.txt";
-        const string notesUrl = "http://media.steampowered.com/apps/steamvr/unityplugin-v{0}.txt";
-        const string pluginUrl = "http://u3d.as/content/valve-corporation/steam-vr-plugin";
+        const string versionUrl = "https://media.steampowered.com/apps/steamvr/unitypluginversion.txt";
+        const string notesUrl = "https://media.steampowered.com/apps/steamvr/unityplugin-v{0}.txt";
+        const string pluginUrl = "https://u3d.as/content/valve-corporation/steam-vr-plugin";
         const string doNotShowKey = "SteamVR.DoNotShow.v{0}";
 
         static bool gotVersion = false;
